3e9b6f9dc87bf386022ad68a03efb57c4c23f67f
unknown <ibanezrg120@yandex.ru>
2022-06-09 14:13:31 +0300

    Initial commit

---
+++ comparison.py
@@ -0,0 +1,13 @@
+a = int(input('Введите число а '))
+b = int(input('Введите число b '))
+
+def compare (a,b):
+
+    x = True
+    y = False
+    if a > b:
+        print(x)
+    else:
+        print(y)
+
+compare(a,b)
