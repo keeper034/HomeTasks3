910c90bab92a874c125b9e44e8a762458c81fbfb
keeper034 <107176421+keeper034@users.noreply.github.com>
2022-06-09 17:16:23 +0300

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,13 +1,15 @@
-a = int(input('Введите число а '))
-b = int(input('Введите число b '))
+import calendar
 
-def compare (a,b):
+print("Введите дату типа 05/2022 ")
+def get_last_friday(inputDate):
+    f = inputDate.split('/')
+    friday = calendar.FRIDAY
+    year = int(f[1])
+    month = int(f[0])
+    d = calendar.monthcalendar(year, month)
+    calc_day = d[-1][friday] or d[-2][friday]
+    rslt = str(calc_day) + '.' + str(month) + '.' + str(year)
+    print(rslt)
+    return rslt
 
-    x = True
-    y = False
-    if a > b:
-        print(x)
-    else:
-        print(y)
-
-compare(a,b)
+get_last_friday(input(str()))
